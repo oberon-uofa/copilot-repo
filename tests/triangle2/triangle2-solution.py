4899e02d0f3e2099603b4c2a0e7bab3bec0e04f4
oberon <alexanderecooper@gmail.com>
2022-09-18 12:14:33 -0400

    commit

--- tests/triangle2/triangle2-solution.py
+++
@@ -1,8 +0,0 @@
-def triangle2(n):
-    if n <= 0:
-        return []
-    if n == 1:
-        return [[1]]
-    list = triangle2(n-1)
-    list.append([m for m in range(list[-1][-1] + 1, list[-1][-1] + n + 1)])
-    return list
